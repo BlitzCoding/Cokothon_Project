578590847a4a6c200032c04793b952272d1c283d
genieu99 <yujin0419@sookmyung.ac.kr>
2022-12-29 08:17:09 +0900

    fix: 구현 거의 완료

--- Youth-is-Challenge-BE/src/main/java/com/jojoldu/book/freelecspringboot2webservice/config/auth/SecurityConfig.java
+++ Youth-is-Challenge-BE/src/main/java/com/jojoldu/book/freelecspringboot2webservice/config/auth/SecurityConfig.java
@@ -14,16 +14,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomOAuth2UserService customOAuth2UserService;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
                 .authorizeRequests()
                 .antMatchers("/", "/css/**", "/scripts/**","/plugin/**", "/h2-console/**", "/fonts/", "/loginPage", "/loginPage/").permitAll()
                 .and()
                     .logout()
                 .and()
-                //[OAuth2 로그인 기능] 설정 진입점
                     .oauth2Login()
-                    .userInfoEndpoint() //-> 여러 설정 중 로그인 성공 이후 사용자 정보 가져올 때 설정
-                    .userService(customOAuth2UserService); //소셜 로그인 성공 시 후속조치 진행할 UserService 인터페이스 구현체 등록
+                    .userInfoEndpoint()
+                    .userService(customOAuth2UserService);
     }
 
     @Override
