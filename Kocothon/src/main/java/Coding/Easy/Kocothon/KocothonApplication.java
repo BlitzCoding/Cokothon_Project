6618d80cf406cf6f2dd7da4357b73d8d5fa80374
iyonghun <cjswl1357@gmail.com>
2022-12-28 15:12:13 +0900

    Koco

---
+++ Kocothon/src/main/java/Coding/Easy/Kocothon/KocothonApplication.java
@@ -0,0 +1,13 @@
+package Coding.Easy.Kocothon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KocothonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KocothonApplication.class, args);
+	}
+
+}
---
+++ Kocothon/src/test/java/Coding/Easy/Kocothon/KocothonApplicationTests.java
@@ -0,0 +1,13 @@
+package Coding.Easy.Kocothon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KocothonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
